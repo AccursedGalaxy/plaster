2f68f616a4cc0267a8793ac17665299b1c4124d2
AccursedGalaxy <robinbohrer7@gmail.com>
2026-03-19 13:42:51 +0100

    remove hello and goodbye commands

--- plaster/main.py
+++ plaster/main.py
@@ -13,19 +13,5 @@ def fetch():
     pass
 
 
-@app
-@app.command()
-def hello(name: str):
-    print(f"Hello {name}")
-
-
-@app.command()
-def goodbye(name: str, formal: bool = False):
-    if formal:
-        print(f"Goodbye Ms. {name}. Have a good day.")
-    else:
-        print(f"Bye {name}!")
-
-
 if __name__ == "__main__":
     app()
